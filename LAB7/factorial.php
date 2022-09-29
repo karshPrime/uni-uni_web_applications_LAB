5309d87953bb9ecd44d4bdad593d76703778eacc
ut-kr <utkarsh@cyberfear.com>
2022-09-29 14:50:18 +1000

    ✨ task 2 step 1 complete

---
+++ LAB7/factorial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial PHP</title>
+</head>
+<body>
+    <?php
+        include ("mathfunction.php");
+    ?>
+
+    <h1>Creating Web Applications - Lab 8</h1>
+
+    <?php
+    	if(isset($_GET["number"])) {
+    		$num = $_GET["number"];
+    		if (isPositiveInteger($num)) {
+    			echo "<p>", $num, "! is ", factorial ($num), ".</>";
+    		} else {
+    			echo "<p>Please enter a positive integer.</p>";
+    		}
+    		echo "<p><a href='index.html'>Return to the Entry Page</a></p>";
+    	}
+    ?>
+</body>
+</html>

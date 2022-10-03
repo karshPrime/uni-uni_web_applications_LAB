2f7f0349793cee1a244b4ee484ebf6f6344f3cf2
ut-kr <utkarsh@cyberfear.com>
2022-10-03 12:13:13 +1100

    ✨ step 4

--- LAB8/processbooking.php
+++ LAB8/processbooking.php
@@ -10,21 +10,32 @@
     <h1>Rohirrim Tour Booking Confirmation</h1>
     <?php
         // check if process was triggered by a form submit, if not display an error message
-        if (isset($_POST["firstname"])) {
+        if ( isset($_POST["firstname"]) ) {
             $firstname = $_POST["firstname"];
-            echo "<p>This is a test: You first name is $firstname</p>";
-
-            if (isset($_POST["lastname"])) $lastname = $_POST["lastname"];
-            echo "<p>This is a test: Your lastname is $lastname";
-
-            if (isset($_POST["age"])) $age = $_POST["age"];
-            echo "<p>This is a test: Your age is $age";
-
-            if (isset($_POST["food"])) $food = $_POST["food"];
-            echo "<p>This is a test: Your food is $food";
-
+            if (isset($_POST["lastname"]))  $lastname = $_POST["lastname"];
+            if (isset($_POST["age"]))       $age  =  $_POST["age"];
+            if (isset($_POST["food"]))      $food =  $_POST["food"];
             if (isset($_POST["partySize"])) $partySize = $_POST["partySize"];
-            echo "<p>This is a test: Your partySize is $partySize";
+
+            // tour
+            $tour = "";
+            if (isset($_POST["1day"]))   $tour = $tour. "1 day ";
+            if (isset($_POST["4day"]))   $tour = $tour. "and 4 days ";
+            if (isset($_POST["10day"]))  $tour = $tour. "and 10 days ";
+
+            // species
+            if (isset($_POST["species"])) {
+                $species = $_POST["species"];
+            } else {
+                $species = "Unknown Species";
+            }
+            
+            // print data
+            echo "<p>Welcome $firstname $lastname</p>";
+            echo "<p> You are booked on the $tour tours</p>";
+            echo "<p> Species: $species</p>";
+            echo "<p> Meal Preference: $food</p>";
+            echo "<p> Number of travellers: $partySize</p>";
 
         } else {
             // Display error message, if process not triggered by a form submit
